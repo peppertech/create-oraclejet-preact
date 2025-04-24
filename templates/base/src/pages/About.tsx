747ff4704a2574b71aaad5f61c00f4960ba0b691
jabrock <john.brock@oracle.com>
2025-04-24 14:01:30 -0700

    styling and theming changes

--- templates/base/src/pages/About.tsx
+++ templates/base/src/pages/About.tsx
@@ -1,8 +1,12 @@
+
+import {Button} from "@oracle/oraclejet-preact/UNSAFE_Button"
+
 export function About() {
 	return (
 		<section>
 			<h1>404: Not Found</h1>
 			<p>It's gone :(</p>
+			<Button label='Click Me' onAction={() => alert("this was clicked")} variant='danger' />
 		</section>
 	);
 }
--- templates/base/src/pages/Home/index.tsx
+++ templates/base/src/pages/Home/index.tsx
@@ -1,4 +1,5 @@
 import preactLogo from '../../assets/preact.svg';
+import jetLogo from '../../assets/jetLogo.svg';
 import './style.css';
 
 export function Home() {
@@ -7,17 +8,21 @@ export function Home() {
 			<a href="https://preactjs.com" target="_blank">
 				<img src={preactLogo} alt="Preact logo" height="160" width="160" />
 			</a>
-			<h1>Get Started building Vite-powered Preact Apps </h1>
+			 
+			<a href="https://oracle.com/jet" target="_blank">
+				<img src={jetLogo} alt="Oracle JET logo" height="160" width="160" />
+			</a>
+			<h1>Get Started building Vite-powered Preact and Oracle JET Apps </h1>
 			<section>
+				<Resource
+					title="Learn Oracle JET"
+					description="To learn more about Oracle JavaScript Extension Toolkit (JET), visit the VDOM Architecture page on the Oracle JET website"
+					href="https://oracle.com/jet"
+				/>
 				<Resource
 					title="Learn Preact"
 					description="If you're new to Preact, try the interactive tutorial to learn important concepts"
 					href="https://preactjs.com/tutorial"
-				/>
-				<Resource
-					title="Differences to React"
-					description="If you're coming from React, you may want to check out our docs to see where Preact differs"
-					href="https://preactjs.com/guide/v10/differences-to-react"
 				/>
 				<Resource
 					title="Learn Vite"
